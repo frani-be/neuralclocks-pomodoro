c7a3191938d5a634f005c53f60a86e600be29dd9
Francisca Beatriz Medina Concha <fbmedinaconcha@gmail.com>
2024-02-26 13:54:21 -0300

    implement visual progress indication for range inputs

--- src/components/RangeInput.jsx
+++ src/components/RangeInput.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
 const RangeInput = ({ label, value, onChange, disabled, inputFor }) => {
+    const handleSliderChange = (event) => {
+        onChange(event.target.value)
+    }
+
     const inputClass = `form-range range-${inputFor}`
     return (
         <div className="my-3 w-100">
-            <label for={inputFor} className="w-100">
+            <label htmlFor={inputFor} className="w-100">
                 {label}: {value} minutes
-                <input type="range" min="1" max="60" value={value} onChange={(e) => onChange(e.target.value)} disabled={disabled} id={inputFor} className={inputClass} aria-label={label} />
+                <input type="range" min="1" max="60" value={value} onChange={handleSliderChange} disabled={disabled} id={inputFor} className={inputClass} aria-label={label} style={{ '--track-fg-width': `${value * 100 / 60}%` }} />
             </label>
         </div>
     )

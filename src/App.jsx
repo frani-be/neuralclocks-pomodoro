53e3aa115d52e0fee8846ac5b780a243a0c43e55
Francisca Beatriz Medina Concha <fbmedinaconcha@gmail.com>
2024-02-27 11:38:02 -0300

    improve mobile utilities for style elements

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ function App() {
     <>
       <header className="row">
         <div className="container">
-          <h1 className="text-center mt-5 mb-4">Pomodoro Timer</h1>
+          <h1 className="text-center mt-xl-5 mb-xl-4 mt-4 mb-2">Pomodoro Timer</h1>
         </div>
       </header>
       <TimerSettingsProvider>
-        <main className="container my-5">
+        <main className="container my-xl-5 mt-3 mb-5">
           <div className="d-flex flex-column flex-xl-row justify-content-center align-items-center align-items-xl-start gap-5 ">
-            <section id="timer" className="d-flex flex-column justify-content-center align-items-center pt-4 gap-3">
+            <section id="timer" className="d-flex flex-column justify-content-center align-items-center pt-xl-4 gap-3">
               <Timer />
             </section>
             <section id="settings" className="d-flex flex-column justify-content-start align-items-start bg-white shadow-sm pt-4 pb-5 px-4 rounded-4 col-xl-4 col-md-8 col-12 mb-5 mb-xl-0 gap-3">
